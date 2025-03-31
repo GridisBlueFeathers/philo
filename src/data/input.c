eee12c23795ecd8261649565e92e2408f0bc942f
Serhii Veretennikov <gridis72@gmail.com>
2025-03-31 12:03:40 +0200

    fix: added protections to reject 0 values for arguments

--- src/data/input.c
+++ src/data/input.c
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:12:08 by svereten          #+#    #+#             */
-/*   Updated: 2025/03/20 15:14:45 by svereten         ###   ########.fr       */
+/*   Updated: 2025/03/31 11:58:31 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philo.h"
@@ -38,6 +38,10 @@ t_bool	input_processing(int32_t argc, char **argv, t_data *data)
 		|| !philo_atoi(argv[4], &data->tts))
 		return (putstr_fd("Invalid arguments\n", STDERR_FILENO), FALSE);
 	if (argv[5] && !philo_atoi(argv[5], &data->times_to_eat))
+		return (putstr_fd("Invalid arguments\n", STDERR_FILENO), FALSE);
+	if (!data->num || !data->ttd || !data->tte || !data->tts)
+		return (putstr_fd("Invalid arguments\n", STDERR_FILENO), FALSE);
+	if (argv[5] && !data->times_to_eat)
 		return (putstr_fd("Invalid arguments\n", STDERR_FILENO), FALSE);
 	return (TRUE);
 }
